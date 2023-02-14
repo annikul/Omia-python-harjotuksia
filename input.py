8cd2cb2fd3044ec281826201e21c086f6fdcfb23
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-14 20:17:01 +0200

    omia ohjeita

---
+++ input.py
@@ -0,0 +1,12 @@
+# nimi, sposti, lempinimi on muuttujia
+# muuttujan arvo on se mitä käyttäjä vastaa
+# input on funktio joka ottaa yhden parametrin joka tulostetaan käyttäjälle terminaalissa esim.anna nimesi. 
+# ja palauttaa käyttäjän syötteen muuttujaan
+nimi = input("Anna nimesi: ")
+sposti = input("Anna sähköpostiosoitteesi: ")
+lempinimi = input("Anna lempinimesi: ")
+
+print("Varmistetaan vielä, että tiedot menivät oikein")
+print("Nimesi: " + nimi)
+print("Sähköpostiosoitteesi: " + sposti)
+print("Lempinimesi: " + lempinimi)
