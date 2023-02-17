07ded1a82fb22729823d3852068ef58a9f61ec17
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-17 23:55:53 +0200

    tehty laskutoimituksia

---
+++ keskiarvo.py
@@ -0,0 +1,6 @@
+luku1 = 2.5
+luku2 = -1.25
+luku3 = 3.62
+
+keskiarvo = (luku1 + luku2 + luku3) / 3
+print(f"Keskiarvo: {keskiarvo}")
---
+++ laskutoimituksia.py
@@ -0,0 +1,12 @@
+x = 27  # Nämä luvut voi vaihtaa ja silti laskutoimitukset toimii koska siinä on f
+y = 15
+
+yhteenlasku = (x + y)
+vahennyslasku = (x - y)
+kertolasku = (x * y)
+jakolasku = (x / y)
+
+print(f'{x} + {y} = {yhteenlasku}')
+print(f'{x} - {y} = {vahennyslasku}')
+print(f'{x} * {y} = {kertolasku}')
+print(f'{x} / {y} = {jakolasku}')
