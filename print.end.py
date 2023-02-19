19cbfe2163676b236015394b2bc7a000ae0fac59
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-19 14:56:34 +0200

    testejä

---
+++ print.end.py
@@ -0,0 +1,8 @@
+# Koska kaikkien perässä on ,end='' tulostuu nyt kaikki printit yhdellä riville
+print(5, end='')
+print(" + ", end='')
+print(8, end='')
+print(" - ", end='')
+print(4, end='')
+print(" = ", end='')
+print(5 + 8 - 4)
