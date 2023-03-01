f7d60efeaeaeac796c85efa7a59c23880e55eee2
AnniK <122267866+annikul@users.noreply.github.com>
2023-03-01 11:37:35 +0200

    kolme luvun summa

---
+++ kolmen_luvun_summa.py
@@ -0,0 +1,7 @@
+luku1 = int(input('Ensimmäinen luku: '))
+luku2 = int(input('Toinen luku: '))
+luku3 = int(input('Kolmas luku: '))
+
+summa = luku1 + luku2 + luku3
+
+print (f'Lukujen summa: {summa} ')
