8e447932e0711f3fc577c760dd5d0a41400f421e
AnniK <122267866+annikul@users.noreply.github.com>
2023-03-23 17:22:54 +0200

    pilkun muuttaminen pisteeksi

--- pilkun_muuttaminen_pisteeksti.py
+++ pilkun_muuttaminen_pisteeksti.py
@@ -9,3 +9,21 @@ txt = '65,5'
 x = txt.replace(',', '.')
 
 print(x)
+
+
+# Toinen esimerkki. Muuttaa molempien luvut pisteille
+
+txt = '1,75 cm paino 65,5 kg'
+
+x = txt.replace(',', '.')
+
+print(x)
+
+# Opi lukemaan syntax
+# string.replace(oldvalue, newvalue, count)
+# string on muuttuja eli teksti (käyttäjä syöttää)
+# replace on metodi
+# sitten tulee sulkuihin 3 arvoa eli käyttäjä syöttää ne
+# oldvalue on vanha arvo (pakollinen)
+# newvalue on uusi arvo (pakollinen)
+# count on määrä (vapaaehtonen)
